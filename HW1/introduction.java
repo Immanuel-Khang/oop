15329f61735d02cb74383c7e4f7fbd231e34b80b
Immanuel-Khang <dinhkhangduong2020@gmail.com>
2025-09-30 22:57:05 +0700

    add introduction.java

---
+++ HW1/introduction.java
@@ -0,0 +1,18 @@
+public class introduction {
+    public static void main(String[] args) {
+        System.out.println("Duong Dinh Khang\t"
+                            + "24020168\t"
+                            + "K69I-IT6\t"
+                            + "Immanuel-Khang\t"
+                            + "dinhkhangduong2020@gmail.com\t"
+                            + '\n'
+                            + "9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "Take one down, pass it around, \n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "...\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "No more bottles of beer on the wall.");
+    }
+}
